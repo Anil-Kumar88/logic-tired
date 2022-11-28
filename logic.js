1b26d1b224796c6d6cd695d4fabc3696bf7a928f
anil kumar <kumar.anilc6@gmail.com>
2022-11-28 09:27:03 +0530

    logic committed

--- logic.js
+++ logic.js
@@ -1,5 +1,3 @@
-
-
 // !area of a circle
 
 // let area = 0;
@@ -117,3 +115,135 @@
 //   result = n1 + n2;
 // }
 // console.log(result);
+
+// ! character count
+
+// let str =prompt("enter the character")
+// let count = 0;
+
+// for (let i = 0; i < str.length; i++) {
+//   count++;
+// }
+
+// console.log(count);
+
+//! combine array.
+// let array1 = ["h", "e", "l", "l", "o"];
+// let array2 = ["w", "o", "r", "l", "d"];
+// let array3 = array1.concat(array2);
+// console.log(array3);
+
+//! program to convert decimal to binary
+
+// take input
+// const number = parseInt(prompt("Enter a decimal number: "));
+
+// // convert to binary
+// const result = number.toString(2);
+
+// console.log("Binary:" + " " + result);
+
+//! descending array.
+// let array = [10, 98, 54, 23, 78, 45];
+// for (let i = 1; i < array.length; i++) {
+//   for (let j = 0; j < i; j++) {
+//     if (array[i] > array[j]) {
+//       let temp = array[i];
+//       array[i] = array[j];
+//       array[j] = temp;
+//     }
+//   }
+// }
+// console.log(array);
+
+//! 46. Find Average of given array
+
+// var arr = [10, 20, 30];
+// var sum = 0;
+// var avg;
+// for (var i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+// }
+// avg = sum / arr.length;
+// console.log(avg);
+
+// //! 16. Program for Calculator
+
+// const operator = prompt("Enter operator ( either +, -, * or / ): ");
+// const number1 = parseFloat(prompt("Enter first number: "));
+// const number2 = parseFloat(prompt("Enter second number: "));
+
+// let result;
+// if (operator == "+") {
+//   result = number1 + number2;
+// } else if (operator == "-") {
+//   result = number1 - number2;
+// } else if (operator == "*") {
+//   result = number1 * number2;
+// } else {
+//   result = number1 / number2;
+// }
+// console.log(`${number1} ${operator} ${number2} = ${result}`);
+
+// ! 35. program to find the ASCII value of a character
+
+// const string = prompt("Enter a character: ");
+// const result = string.charCodeAt(0);
+
+// console.log(`The ASCII value is: ${result}`);
+
+//! program to find the ASCII value of a character
+
+// take input from the user
+// const string = prompt('Enter a character: ');
+
+// convert into ASCII value
+// const result = string.codePointAt(0);
+
+// console.log(`The ASCII value is: ${result}`);
+
+// //! 13. Program to sort array in Ascending Order
+
+// var array = [10, 98, 54, 23, 78, 45];
+// for (let i = 1; i < array.length; i++) {
+//   for (let j = 0; j < i; j++) {
+//     if (array[i] < array[j]) {
+//       let temp = array[i];
+//       array[i] = array[j];
+//       array[j] = temp;
+//     }
+//   }
+// }
+// console.log(array);
+
+//! 1.Find Average of given array
+
+// var arr = [40, 50, 60];
+// var sum = 0;
+// var avg;
+// for (var i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+// }
+// avg = sum / arr.length;
+// console.log(avg);
+
+// ! 3. program to find the ASCII value of a character
+
+// const string = prompt("Enter a character: ");
+// const result = string.charCodeAt(0);
+
+// console.log(`The ASCII value is: ${result}`);
+
+//! 4. Program to sort array in Ascending Order
+
+// var array = [10, 98, 54, 23, 78, 45];
+// for (let i = 1; i < array.length; i++) {
+//   for (let j = 0; j < i; j++) {
+//     if (array[i] < array[j]) {
+//       let temp = array[i];
+//       array[i] = array[j];
+//       array[j] = temp;
+//     }
+//   }
+// }
+// console.log(array);
